3c9771a0c28bb4f527400d3ccc9dec8c4005d935
aoliveira <andreluiz1088@hotmail.com>
2025-03-17 21:58:07 -0300

    fix: adjust the database connection to include the schema

--- src/pkg/database/db.go
+++ src/pkg/database/db.go
@@ -23,9 +23,10 @@ func ConnectDB() (err error) {
 	host := os.Getenv("DB_HOST")
 	port := os.Getenv("DB_PORT")
 	name := os.Getenv("DB_NAME")
+	schema := os.Getenv("DB_SCHEMA")
 
 	// Montar DSN (Data Source Name)
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", host, user, password, name, port)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable search_path=%s", host, user, password, name, port, schema)
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
