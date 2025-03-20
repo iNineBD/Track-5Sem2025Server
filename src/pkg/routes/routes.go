7aaba9afc71717d50d71e20e86e490ef9953b81d
William Antoniazzi <william.antoniazzi@outlook.com>
2025-03-20 19:59:21 -0300

    Golangci-ling working

--- src/pkg/routes/routes.go
+++ src/pkg/routes/routes.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	// Import necessário para gerar documentação com Swagger
 	_ "inine-track/docs"
+	"log"
 
 	"github.com/gin-gonic/gin"
 	swaggerFiles "github.com/swaggo/files"
@@ -19,5 +21,7 @@ func HandlleRequest() {
 	// Endpoint Swagger
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	r.Run()
+	if err := r.Run(); err != nil {
+		log.Fatalf("Erro ao iniciar o servidor: %v", err)
+	}
 }
