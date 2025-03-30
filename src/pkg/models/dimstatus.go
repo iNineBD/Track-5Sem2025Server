f87da85f011b126bfb42669230b67bcac9057308
aoliveira <andreluiz1088@hotmail.com>
2025-03-29 23:51:53 -0300

    TRK-93 bugfix: adjustments to metrics return

--- src/pkg/models/dimstatus.go
+++ src/pkg/models/dimstatus.go
@@ -1,8 +1,10 @@
 package models
 
 type DimStatus struct {
-	ID   int64  `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name string `json:"name"`
+	Id         int64  `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name       string `json:"name"`
+	Id_card    int64  `json:"id_card"`
+	Id_project int64  `json:"id_project"`
 }
 
 func (DimStatus) TableName() string {
