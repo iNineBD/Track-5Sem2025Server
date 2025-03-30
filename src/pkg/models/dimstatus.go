0decc737e74accb0582785caff0539c7f9d8e34f
Anaraquely <anaraquelysmachado29@gmail.com>
2025-03-30 18:23:51 -0300

    fix: rename variables to Go pattern

--- src/pkg/models/dimstatus.go
+++ src/pkg/models/dimstatus.go
@@ -1,10 +1,10 @@
 package models
 
 type DimStatus struct {
-	Id         int64  `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name       string `json:"name"`
-	Id_card    int64  `json:"id_card"`
-	Id_project int64  `json:"id_project"`
+	ID        int64  `json:"id" gorm:"primaryKey;autoIncrement"`
+	Name      string `json:"name"`
+	IdCard    int64  `json:"id_card"`
+	IdProject int64  `json:"id_project"`
 }
 
 func (DimStatus) TableName() string {
