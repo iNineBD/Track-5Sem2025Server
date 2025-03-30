b6085035ed8aea6cae27a1a0d392024da590d754
Anaraquely <anaraquelysmachado29@gmail.com>
2025-03-30 18:45:16 -0300

    fix: rename variables to Go pattern

--- src/pkg/models/dimtag.go
+++ src/pkg/models/dimtag.go
@@ -4,8 +4,8 @@ type DimTag struct {
 	ID        int64  `json:"id" gorm:"primryKey;autoIncrement"`
 	Name      string `json:"name"`
 	Color     string `json:"color"`
-	IdCard    int64  `json:"id_card"`
-	IdProject int64  `json:"id_project"`
+	IDCard    int64  `json:"id_card"`
+	IDProject int64  `json:"id_project"`
 }
 
 func (DimTag) TableName() string {
