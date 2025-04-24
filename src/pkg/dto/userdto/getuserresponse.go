582434d94c40fda9f8152cdb850579e995a55ea0
Anaraquely <anaraquelysmachado29@gmail.com>
2025-04-23 23:53:15 -0300

    fix: removing the . in the final of the Role line

--- src/pkg/dto/userdto/getuserresponse.go
+++ src/pkg/dto/userdto/getuserresponse.go
@@ -1,9 +1,8 @@
 package userdto
 
-
 type GetUserResponse struct {
-	ID int64 `json:"id_user,omitempty"`
+	ID    int64  `json:"id_user,omitempty"`
 	Email string `json:"email,omitempty"`
-	Role int64 `json:"id_role,omitempty"`.
+	Role  int64  `json:"id_role,omitempty"`
 	Token string `json:"token,omitempty"`
 }
