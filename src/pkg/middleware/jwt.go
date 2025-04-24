1192b45f70975eeaf172c6ac50c078a1c39e4afa
Anaraquely <anaraquelysmachado29@gmail.com>
2025-04-23 23:48:41 -0300

    feat: implements the main functions to generate JWT tokens

---
+++ src/pkg/middleware/jwt.go
@@ -0,0 +1,59 @@
+package middleware
+
+import (
+	"inine-track/pkg/config"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+)
+
+// Gera token JWT
+func GenerateJWT(userID int64, email string, role int64) (string, error) {
+	jwtKey, err := config.LoadJWTKey()
+	if err != nil {
+		return "erro ao carregar JWT", err
+	}
+	claims := jwt.MapClaims{
+
+		"user_id": userID,
+		"email":   email,
+		"role":    role,
+		"exp":     time.Now().Add(1 * time.Hour).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(jwtKey))
+}
+
+// Middleware para validar o token JWT
+func JWTMiddleware() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Token não fornecido"})
+			return
+		}
+
+		tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+		token, err := jwt.Parse(tokenStr, func(t *jwt.Token) (interface{}, error) {
+			jwtKey, err := config.LoadJWTKey()
+			if err != nil {
+				return "erro ao carregar JWT", err
+			}
+			if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrSignatureInvalid
+			}
+			return jwtKey, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Token inválido"})
+			return
+		}
+
+		c.Next()
+	}
+}
